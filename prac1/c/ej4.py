fcf6987e1dff2d35bb8b79168d753bcc505794b7
HyCris <96534422+HyCrisXXI@users.noreply.github.com>
2026-02-22 13:04:55 +0100

    added e1.c and edited ej4.py

--- prac1/c/ej4.py
+++ prac1/c/ej4.py
@@ -1,5 +1,4 @@
 from socket import *
-from http.server import HTTPServer, BaseHTTPRequestHandler
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
@@ -7,16 +6,37 @@ serverSocket.bind(('localhost', 8000))
 
 serverSocket.listen(1)
 
-print("Preparado para servir...")
+
+sock_name = serverSocket.getsockname()
+base_url = str(sock_name[0]) + ":" + str(sock_name[1])
+
+print("Sirviendo en:", base_url)
 while True:
     connectionSocket, addr = serverSocket.accept()
-    request = connectionSocket.recv(4096)
-    
-    for line in request:
-        pass
-    
-    connectionSocket.sendall((header + body_response).encode())
-    print(request.decode())
-    
+    request = b""
+
+    while True:
+        data = connectionSocket.recv(4096)
+        if not data:
+            break
+        request += data
+
+        if b"\r\n\r\n" in request:
+            break
+
+    request_str = request.decode()
+
+    url = request_str.split("GET ", 1)[1].split()[0]
+    print (request_str)
+    for line in request_str.split('\n'):
+        if "User-Agent: " in line:
+            browser = line.split("User-Agent: ", 1)[1]
+            break
+
+    response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<h1>Hola Mundo</h1>" \
+    "<b>La dirección URL pedida es: " + base_url + url + "<br>Su buscador es: " + browser + "</b>"
+
+    connectionSocket.send(response.encode(encoding="ISO-8859-1"))
+
     connectionSocket.shutdown(SHUT_RDWR)
     connectionSocket.close()
