1adac56a273d80592cb05c4e7c55a76a512a43e0
HyCris <96534422+HyCrisXXI@users.noreply.github.com>
2026-02-26 17:41:41 +0100

    fix: c/ej6 now returns only words (register-format)

--- prac1/c/ej6.py
+++ prac1/c/ej6.py
@@ -4,13 +4,12 @@ import sys
 if(len(sys.argv) < 2):
     print("Uso: python3 e6.py <numero>")
 
-num = int(sys.argv[1])
-
 try:
-    b1 = struct.pack("<B", num)
-    b2 = struct.pack("<H", num)
+    num = int(sys.argv[1], 0)
+
     b4 = struct.pack("<I", num)
-except struct.error:
-    print("Introduzca un número en el rango [0, 255]")
-    sys.exit()
-sys.stdout.buffer.write(b1 + b2 + b4)
+    sys.stdout.buffer.write(b4)
+
+except Exception as e:
+    sys.stderr.write(f"Error: {e}\n")
+
