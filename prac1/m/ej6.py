1ca18af506ba17adc7f4034f3c70898a7d2096ac
HyCris <96534422+HyCrisXXI@users.noreply.github.com>
2026-03-04 22:04:09 +0100

    small change

--- prac1/m/ej6.py
+++ prac1/m/ej6.py
@@ -9,10 +9,8 @@ try:
 except ValueError:
     sys.exit("El argumento debe ser un número entero")
 
-    b1 = struct.pack('<B', num & 0xFF)
-    b2 = struct.pack('<H', num & 0xFFFF)
-    b4 = struct.pack('<I', num & 0xFFFFFFFF)
+b1 = struct.pack('<B', num & 0xFF)
+b2 = struct.pack('<H', num & 0xFFFF)
+b4 = struct.pack('<I', num & 0xFFFFFFFF)
 
-sys.stdout.buffer.write(b1)
-sys.stdout.buffer.write(b2)
-sys.stdout.buffer.write(b4)
+sys.stdout.buffer.write(b1 + b2 + b4)
