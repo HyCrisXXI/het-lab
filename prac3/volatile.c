d7eb177fc0812c015e3a092b59a22c2722bcf69d
HyCrisXXI <96534422+HyCrisXXI@users.noreply.github.com>
2026-03-22 18:27:28 +0100

    better volatile.c

--- prac3/volatile.c
+++ prac3/volatile.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-volatile int la_global;
+volatile int la_global = 1;
 
 int main() {
-	volatile int la_local;
-	printf("Variable global direccion: %p\n", &la_global);
-	printf("Variable local direccion: %p\n", &la_local);
+	volatile int la_local = 2;
+	int i = 0;
+	while(i < 10) {
+		i++;
+	}
+	// printf("%d", i); // para que el compilador no elimine el bucle
 	return 0;
 }
